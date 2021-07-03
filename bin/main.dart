90e5d99923c73ba098f28cb6378b6488a0eae8c6
Raj <rajkamalapps1999@gmail.com>
2021-07-03 11:25:26 +0530

    hello

---
+++ bin/main.dart
@@ -0,0 +1,51 @@
+void main() {
+  // int number = 10;
+  // print(number);
+
+  // var n = 10;
+  // print(n);
+
+  // String name = 'Raj';
+  // print(name);
+
+  // var hello = true;
+  // print(hello.runtimeType);
+
+  // var i = 10;
+  // print(i);
+  // print(i++);
+  // print(i);
+  // print(++i);
+
+  // int i = 10;
+  // var num1 = 20;
+  // print(i < num1);
+  // print(i is! int);
+
+  // var isLogin = false;
+  // var user = isLogin ? 'Raj' : "Guest";
+  // print(user);
+
+  // var raj = 'raj';
+  // var user1 = raj ?? 'Guest';
+  // print(user1);
+  // print('this is ${raj}');
+
+  // var s1 = 'Raj';
+  // var s2 = 'kamal';
+  // print(s1 + s2);
+
+  // var lst = ['raj', 'kamal', '100'];
+  // print(lst.runtimeType);
+
+  // var women = ["rani", "sonam", "shweta"];
+  // var men = ["Rahul", "Vikash"];
+  // var menandWomen = ["Men&Women", ...men, ...women];
+  // print(menandWomen);
+
+  List <String>student = [];
+  student.add("raj");
+  print(student);
+
+
+}
